49c42198659ba099370bfa261255a3531f826fa5
Tewodros Abebe <teddyab729@gmail.com>
2023-01-20 23:50:20 +0300

    Sorting the Sentence

---
+++ Leetcode/SortingTheSentence.js
@@ -0,0 +1,31 @@
+var sortSentence = function(s) {
+
+    s = s.split(' ');
+
+    let temp = [];
+    for (let i = 0; i < s.length; i++) {
+        temp.push(s[i].split('').reverse().join(''));
+    }
+
+    temp.sort()
+
+    result = [];
+    for (let j = 0; j < s.length; j++) {
+        result.push(temp[j].slice(1).split('').reverse().join(''))
+    }
+
+    return result.join(' ');
+};
+
+// (runtime / memory)
+//  76 ms / 38.8 MB
+
+
+
+var sortSentence = function(s) {
+
+    return s.split(' ')
+        .sort((a, b) => a[a.length - 1] - b[b.length - 1])
+        .map((word) => word.slice(0, word.length - 1))
+        .join(' ');
+};
