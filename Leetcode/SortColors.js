a993a47681ed9238ecf48856238b1b40622d0a75
Tewodros Abebe <teddyab729@gmail.com>
2023-01-23 00:10:01 +0300

    Sort Colors

---
+++ Leetcode/SortColors.js
@@ -0,0 +1,26 @@
+var sortColors = function (nums) {
+  let zeros = 0;
+  let ones = 0;
+  let twos = 0;
+  let N = nums.length;
+  for (let i = 0; i < N; i++) {
+    if (nums[i] === 0) {
+      zeros++;
+    } else if (nums[i] === 1) {
+      ones++;
+    } else {
+      twos++;
+    }
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (i < zeros) {
+      nums[i] = 0;
+    } else if (i >= zeros && i < zeros + ones) {
+      nums[i] = 1;
+    } else {
+      nums[i] = 2;
+    }
+  }
+  return nums;
+};
