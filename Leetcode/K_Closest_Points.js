ecd38a838c8c725e125a80c28b7bb9b910b7e484
Tewodros Abebe <teddyab729@gmail.com>
2023-01-23 00:43:49 +0300

    K Closest

---
+++ Leetcode/K_Closest_Points.js
@@ -0,0 +1,6 @@
+const kClosest = (points = [], K) => {
+    const sorted = points.sort(
+      ([a, b], [c, d]) => Math.sqrt(a * a + b * b) - Math.sqrt(c * c + d * d),
+    )
+    return sorted.slice(0, K)
+  }
