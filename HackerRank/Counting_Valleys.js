f462bd49bbd9e489a408580ba435d88c0c90e493
Tewodros Abebe <teddyab729@gmail.com>
2023-01-25 18:58:29 +0300

    Hacker Rank

--- HackerRank/Counting_Valleys.js
+++ HackerRank/Counting_Valleys.js
@@ -32,33 +32,18 @@ function readLine() {
  */
 
 function countingValleys(n, s) {
-  const min = 2;
-  const max = 1000000;
-  let isInValley = false;
   let valleys = 0;
-  s = typeof s === "string" ? s.split("") : s;
-
-  if (
-    s.length >= min &&
-    s.length <= max &&
-    n === parseInt(n, 0) &&
-    n >= min &&
-    n <= max &&
-    n === s.length
-  ) {
-    s.map((steps) => (steps === "U" ? 1 : -1)).reduce((prev, next) => {
-      if (prev < 0 && !isInValley) {
-        isInValley = true;
-      }
-      if (prev + next === 0 && isInValley) {
+  let elevation = 0;
+  for (let i = 0; i < n; i++) {
+    if (s[i] == "D") {
+      elevation--;
+    } else {
+      if (elevation == -1) {
         valleys++;
-        isInValley = false;
       }
-
-      return prev + next;
-    });
+      elevation++;
+    }
   }
-
   return valleys;
 }
 
