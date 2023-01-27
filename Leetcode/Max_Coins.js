3846b294bdbce27c767b01270e663f2235d17689
Tewodros Abebe <teddyab729@gmail.com>
2023-01-27 22:55:12 +0300

    Leet code

--- Leetcode/Max_Coins.js
+++ Leetcode/Max_Coins.js
@@ -7,10 +7,8 @@ var maxCoins = function (piles) {
   let result = 0;
   len = piles.length * (2 / 3);
   piles.sort((a, b) => b - a);
-  for (let i = 0; i < len; i++) {
-    if (i % 2 === 1) {
-      result += piles[i];
-    }
+  for (let i = 1; i < len; i += 2) {
+    result += piles[i];
   }
   return result;
 };
