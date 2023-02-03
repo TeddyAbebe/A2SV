2deaf37a61fc0c418b07dd221b9523cf7c32398d
Tewodros Abebe <teddyab729@gmail.com>
2023-02-03 17:49:05 +0300

    Leet Code

--- Leetcode/TaskScheduler.js
+++ Leetcode/TaskScheduler.js
@@ -4,57 +4,27 @@
  * @return {number}
  */
 var leastInterval = function (tasks, n) {
-  const map = new Map();
-  for (const task of tasks.values()) {
-    map.set(task, 1 + (map.get(task) || 0));
-  }
+  //count of each task
+  const countOfTask = new Array(26).fill(0);
 
-  const queue = [];
-  for (const [ch, count] of map.entries()) {
-    const item = { ch, count, nextIndex: -1 };
-    queue.push(item);
-  }
-  queue.sort((A, B) => B.count - A.count);
-
-  let result = 0;
-  for (let i = 0; queue.length; i++, result++) {
-    let useMe = null,
-      j = 0;
-    for (; j < queue.length; j++) {
-      const item = queue[j];
-      if (item.nextIndex <= i) {
-        useMe = item;
-        break;
-      }
-    }
-
-    if (useMe) {
-      useMe.count--;
-      useMe.nextIndex = i + (1 + n);
-
-      if (useMe.count === 0) {
-        queue.splice(j, 1);
-      } else {
-        let low = j + 1,
-          high = -1 + queue.length;
-        if (queue[high].count >= useMe.count) {
-          queue.splice(j, 1);
-          queue.push(useMe);
-        } else {
-          while (low < high) {
-            const mid = Math.floor((low + high) / 2);
-            const midVal = queue[mid].count;
-            if (midVal < useMe.count) high = mid;
-            else low = 1 + mid;
-          }
-          for (let k = j; k < high - 1; k++) {
-            queue[k] = queue[k + 1];
-          }
-          queue[high - 1] = useMe;
-        }
-      }
+  let maxCount = 0;
+  const codeForA = "A".charCodeAt(0);
+  for (let task of tasks) {
+    const index = task.charCodeAt(0) - codeForA;
+    countOfTask[index] += 1;
+    if (countOfTask[index] > maxCount) {
+      maxCount = countOfTask[index];
     }
   }
 
-  return result;
+  //determine how many tasks are to the maxCount
+  let taskCountOfMaxCount = 0;
+  for (let count of countOfTask) {
+    if (count === maxCount) {
+      taskCountOfMaxCount += 1;
+    }
+  }
+
+  const intervalRequired = (maxCount - 1) * (n + 1) + taskCountOfMaxCount;
+  return Math.max(intervalRequired, tasks.length);
 };
