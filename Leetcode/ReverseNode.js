d665c4e123d46822a496687dcf6dcd04026c7c82
Tewodros Abebe <teddyab729@gmail.com>
2023-02-08 21:38:16 +0300

    Leet Code

---
+++ Leetcode/ReverseNode.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function (head, k) {
+  if (!head) return head;
+
+  let curr = head;
+  let count = 0;
+  while (curr && count != k) {
+    curr = curr.next;
+    count++;
+  }
+
+  if (count == k) {
+    curr = reverseKGroup(curr, k);
+    while (count > 0) {
+      let temp = head.next;
+      head.next = curr;
+      curr = head;
+      head = temp;
+      count--;
+    }
+    head = curr;
+  }
+  return head;
+};
